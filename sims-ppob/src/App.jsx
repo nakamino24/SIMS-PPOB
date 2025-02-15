a0b4c0c1038566a7db8c4f34c550a4d80c4802bf
Quways <nakasedu@gmail.com>
2025-02-15 10:10:11 +0700

    Fix main.jsx & Vercel deploy issue

--- sims-ppob/src/App.jsx
+++ sims-ppob/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Home from "./pages/Dashboard/Home";
@@ -6,7 +11,6 @@ import Profile from "./pages/Dashboard/Profile";
 import TopUp from "./pages/Dashboard/TopUp";
 import Transaction from "./pages/Dashboard/Transaction";
 import History from "./pages/Dashboard/History";
-import { Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -14,18 +18,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Navigate to="/register" />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Home />} />
-        <Route path="/profile" element={<Navigate to="/profile" />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/topup" element={<Navigate to="/topup" />} />
         <Route path="/topup" element={<TopUp />} />
-        <Route path="/transaction" element={<Navigate to="/transaction" />} />
         <Route path="/transaction" element={<Transaction />} />
-        <Route path="/history" element={<Navigate to="/history" />} />
         <Route path="/history" element={<History />} />
+        <Route path="*" element={<Navigate to="/login" />} />{" "}
+        {/* Redirect jika halaman tidak ditemukan */}
       </Routes>
     </Router>
   );
